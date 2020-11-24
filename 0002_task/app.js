57402dd19be4e532fddcc22ed78446b18a233770
ramirez <yuta.yamazaki@infobahn.co.jp>
2020-11-24 14:22:44 +0900

    アップロードしなおし

--- 0002_task/app.js
+++ 0002_task/app.js
@@ -1,31 +1,34 @@
 const btn = document.getElementById("btn");
 const target = document.getElementById("task");
+const tasklist = [];
 
 function gettask() {
-    const task = [];
     let newtask = {};
-    let taskid = task.length + 1;
+    let taskid = tasklist.length + 1;
     let taskname = document.getElementById("new-task").value;
     let taskstatus = "作業中";
     newtask.id = taskid;
     newtask.name = taskname;
     newtask.status = taskstatus;
-    task.push(newtask);
+    tasklist.push(newtask);
+    newtask = {};
 }
 
 function add() {
     gettask();
+    console.log(tasklist[0].id)
     let tr = document.createElement('tr');
-    for (i = 0; i <= task.length; i++) {
+    for (i = 0; i < tasklist.length; i++) {
         let tdid = document.createElement('td');
-        td.innerHTML = task[i].taskid;
+        tdid.innerHTML = tasklist[i].id;
         tr.appendChild(tdid);
         let tdname = document.createElement('td');
-        td.innerHTML = task[i].taskname;
+        tdname.innerHTML = tasklist[i].name;
         tr.appendChild(tdname);
         let tdstatus = document.createElement('td');
-        td.innerHTML = task[i].taskstatus;
+        tdstatus.innerHTML = tasklist[i].status;
         tr.appendChild(tdstatus);
     }
+    target.appendChild(tr);
 }
 btn.addEventListener("click", add)
