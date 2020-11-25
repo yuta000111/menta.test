1e3317cfb258a00cef96f324a7a9e887d9c6f7ed
ramirez <yuta.yamazaki@infobahn.co.jp>
2020-11-25 19:08:05 +0900

    諸々修正

--- 0002_task/app.js
+++ 0002_task/app.js
@@ -1,34 +1,30 @@
-const btn = document.getElementById("btn");
-const target = document.getElementById("task");
+const btn = document.getElementById('btn');
 const tasklist = [];
 
 function gettask() {
     let newtask = {};
     let taskid = tasklist.length + 1;
-    let taskname = document.getElementById("new-task").value;
-    let taskstatus = "作業中";
+    let taskname = document.getElementById('new-task').value;
     newtask.id = taskid;
     newtask.name = taskname;
-    newtask.status = taskstatus;
+    newtask.status = '作業中';
     tasklist.push(newtask);
     newtask = {};
 }
 
 function add() {
     gettask();
-    console.log(tasklist[0].id)
+    const target = document.getElementById('task');
     let tr = document.createElement('tr');
-    for (i = 0; i < tasklist.length; i++) {
-        let tdid = document.createElement('td');
-        tdid.innerHTML = tasklist[i].id;
-        tr.appendChild(tdid);
-        let tdname = document.createElement('td');
-        tdname.innerHTML = tasklist[i].name;
-        tr.appendChild(tdname);
-        let tdstatus = document.createElement('td');
-        tdstatus.innerHTML = tasklist[i].status;
-        tr.appendChild(tdstatus);
-    }
+    let tdid = document.createElement('td');
+    tdid.innerHTML = tasklist[tasklist.length - 1].id;
+    tr.appendChild(tdid);
+    let tdname = document.createElement('td');
+    tdname.innerHTML = tasklist[tasklist.length - 1].name;
+    tr.appendChild(tdname);
+    let tdstatus = document.createElement('td');
+    tdstatus.innerHTML = tasklist[tasklist.length - 1].status;
+    tr.appendChild(tdstatus);
     target.appendChild(tr);
 }
-btn.addEventListener("click", add)
+btn.addEventListener('click', add)
