50a5bba10dbb8c16d476709d9a15e38a5ea53c5e
ramirez <yuta.yamazaki@infobahn.co.jp>
2021-02-26 14:54:17 +0900

    今日の進捗

--- vue/vue03/src/App.vue
+++ vue/vue03/src/App.vue
@@ -1,17 +1,36 @@
 <template>
-  <index> </index>
+  <div>
+    <h1>ToDOリスト</h1>
+    <label for="radio01"
+      >すべて<input id="radio01" type="radio" name="radio"
+    /></label>
+    <label for="radio02"
+      >作業中<input id="radio02" type="radio" name="radio"
+    /></label>
+    <label for="radio03"
+      >完了<input id="radio03" type="radio" name="radio"
+    /></label>
+    <table>
+      <tbdoy>
+        <tr>
+          <th>ID</th>
+          <th>コメント</th>
+          <th>完了</th>
+        </tr>
+      </tbdoy>
+    </table>
+    <h3>新規タスクの追加</h3>
+    <input id="newtask" type="text" /><button>追加</button>
+  </div>
 </template>
 
 <script>
-import index from "@/components/index.vue";
 export default {
-  components: {
-    index
-  },
   data: function() {
     return {
       id: "",
-      task: {}
+      task: {},
+      comment: ""
     };
   }
 };
