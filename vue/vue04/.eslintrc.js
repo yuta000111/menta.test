d3bd7f1714cf4d5b55a04ba46e114c94f04551d4
ramirez <yuta.yamazaki@infobahn.co.jp>
2021-03-19 21:17:15 +0900

    kadai 課題4実装

---
+++ vue/vue04/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true
+    },
+    extends: [
+        'eslint:recommended',
+        'plugin:vue/recommended',
+        'prettier',
+        'prettier/vue'
+    ],
+    parserOptions: {
+        parser: 'babel-eslint'
+    },
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    }
+}
